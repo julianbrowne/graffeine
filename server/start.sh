58499c3296ac591923c9f3cf91112d6e0c781d71
julianbrowne <julian@webskill.com>
2016-05-31 18:35:35 +0100

    start script tidy

--- server/start.sh
+++ server/start.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-SCRIPT=$0
-SCRIPT=`basename "$SCRIPT"`
-
-NODE_VERSION="`node -v`"
+#NODE_VERSION="`node -v`"
 NODE_SERVER_PORT=8004
 
 NEO4J_HOME="/usr/local/neo4j"
 NEO4J_SERVER_PORT=7474
 
-GRAFF_HOME="`cd $( dirname "$SCRIPT" )/.. && dirs -l +0`"
+GRAFF_HOME="`cd $( dirname "${0}" )/.. && dirs -l +0`"
 
 # start neo4j
+
+echo "Expecting Neo4J in ${NEO4J_HOME} running on port ${NEO4J_SERVER_PORT}"
 
 neopid=$(lsof -i :${NEO4J_SERVER_PORT} -F T -Ts | grep -i "TST=LISTEN" -B1 | head -n1)
 neopid=${neopid:1}
 
 if [ ${neopid} ]
 then
-    echo "Neo4j already running (${neopid})"
+    echo "Neo4j already running (pid: ${neopid})"
 else
     ${NEO4J_HOME}/bin/neo4j start
 fi
@@ -30,7 +29,7 @@ nodepid=${nodepid:1}
 
 if [ ${nodepid} ]
 then
-    echo "Graffeine already running (${nodepid})"
+    echo "Graffeine already running (pid: ${nodepid})"
 else
     echo "Starting ${GRAFF_HOME}/server/server.js"
     node ${GRAFF_HOME}/server/server.js

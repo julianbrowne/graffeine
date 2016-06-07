bba88fd8ea06eb6c647fd5dff0c6904e8f5a959a
julianbrowne <julian@webskill.com>
2016-06-07 08:09:23 +0100

    removed bit of debug

--- server/lib/models/utils.js
+++ server/lib/models/utils.js
@@ -4,7 +4,6 @@ var util = require("util");
 module.exports = (function() { 
 
     function extractProperties(nodeData) { 
-        console.log(typeof nodeData);
         if(typeof nodeData === "undefined" || nodeData === null) { return {}; }
         var properties = (!('data' in nodeData)) ? {} : nodeData.data;
         return properties;
